d9372efd374a8ccbefdfa02a46f978c82159201a
YunTang1997 <2074402078@qq.com>
2022-04-08 22:01:19 +0800

    构建scala学习项目

---
+++ src/main/scala/com/ty/chapter01/HelloWord.scala
@@ -0,0 +1,20 @@
+package com.ty.chapter01
+
+/**
+ * @author ty
+ * @date 2022/4/8 19:48
+ * @describe 声明一个单例对象HelloWord（伴生对象）
+ */
+
+// object，关键字，声明一个单例对象（伴生对象）
+object HelloWord {
+
+  /*
+    main方法：从外部可以直接调用执行的方法
+    def 方法名称(参数名称: 参数[泛型]): 函数返回值类型 = {方法体}
+   */
+  def main(args: Array[String]): Unit = {
+    println("hello word")
+    System.out.println("run java in scala")
+  }
+}
